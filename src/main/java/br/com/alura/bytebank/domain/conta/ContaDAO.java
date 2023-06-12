e06c20a908808b76941b78a9cc0f9ae607c4247e
andre <andre1993andre@hotmail.com>
2023-06-11 21:59:23 -0300

    incluindo o metodo buscarContaPorNumero

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -1,11 +1,15 @@
 package br.com.alura.bytebank.domain.conta;
 
 import br.com.alura.bytebank.domain.cliente.Cliente;
+import br.com.alura.bytebank.domain.cliente.DadosCadastroCliente;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContaDAO {
 
@@ -33,8 +37,74 @@ public class ContaDAO {
             preparedStatement.setString(5,dadosDaConta.dadosCliente().email());
 
             preparedStatement.execute();
+
+            preparedStatement.close();
+            conn.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Set<Conta> listar(){
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        Set<Conta> contas = new HashSet<Conta>();
+
+        String sql = "SELECT * FROM conta;";
+
+        try {
+            preparedStatement = conn.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                Integer numero = resultSet.getInt(1);
+                BigDecimal saldo = resultSet.getBigDecimal(2);
+                String nome = resultSet.getString(3);
+                String cpf = resultSet.getString(4);
+                String email = resultSet.getString(5);
+
+                DadosCadastroCliente dadosCadastroCliente = new DadosCadastroCliente(nome, cpf, email);
+                Cliente cliente = new Cliente(dadosCadastroCliente);
+
+                contas.add(new Conta(numero, cliente));
+
+            }
+        } catch (SQLException e){
+            throw new RuntimeException();
+        }
+        return contas;
+    }
+
+    public Conta listarPorNumero(Integer numero) {
+        String sql = "SELECT * FROM conta WHERE numero = ?";
+
+        PreparedStatement ps;
+        ResultSet resultSet;
+        Conta conta = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, numero);
+            resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                Integer numeroRecuperado = resultSet.getInt(1);
+                BigDecimal saldo = resultSet.getBigDecimal(2);
+                String nome = resultSet.getString(3);
+                String cpf = resultSet.getString(4);
+                String email = resultSet.getString(5);
+
+                DadosCadastroCliente dadosCadastroCliente =
+                        new DadosCadastroCliente(nome, cpf, email);
+                Cliente cliente = new Cliente(dadosCadastroCliente);
+
+                conta = new Conta(numeroRecuperado, cliente);
+            }
+            resultSet.close();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return conta;
     }
 }
--- src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -2,16 +2,15 @@ package br.com.alura.bytebank.domain.conta;
 
 import br.com.alura.bytebank.ConnectionFactory;
 import br.com.alura.bytebank.domain.RegraDeNegocioException;
-import br.com.alura.bytebank.domain.cliente.Cliente;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContaService {
+
+    private Set<Conta> contas = new HashSet<>();
 
     private ConnectionFactory connection;
 
@@ -19,10 +18,9 @@ public class ContaService {
         this.connection = new ConnectionFactory();
     }
 
-    private Set<Conta> contas = new HashSet<>();
-
     public Set<Conta> listarContasAbertas() {
-        return contas;
+        Connection conn = connection.retornaConexao();
+        return new ContaDAO(conn).listar();
     }
 
     public BigDecimal consultarSaldo(Integer numeroDaConta) {
@@ -31,9 +29,7 @@ public class ContaService {
     }
 
     public void abrir(DadosAberturaConta dadosDaConta) {
-
         Connection conn = connection.retornaConexao();
-
         new ContaDAO(conn).salvar(dadosDaConta);
     }
 
@@ -69,10 +65,12 @@ public class ContaService {
     }
 
     private Conta buscarContaPorNumero(Integer numero) {
-        return contas
-                .stream()
-                .filter(c -> c.getNumero() == numero)
-                .findFirst()
-                .orElseThrow(() -> new RegraDeNegocioException("Não existe conta cadastrada com esse número!"));
+        Connection conn = connection.retornaConexao();
+        Conta conta = new ContaDAO(conn).listarPorNumero(numero);
+        if(conta != null) {
+            return conta;
+        } else {
+            throw new RegraDeNegocioException("Não existe conta cadastrada com esse número!");
+        }
     }
 }
